7b60ede29b3d2ce555e9ed81d355b9c49017c525
Lukasz Bondarewicz <lukasz@parcelvision.com>
2017-04-21 17:49:02 +0100

    .

---
+++ intro.js
@@ -0,0 +1,45 @@
+// Mousetrap.bind('f i x m e', function() { 
+//   document.body.innerHTML = '<p>check the console <span>&#x2588;</span></p>';
+//   console.log('SSB3b3VsZCBsaWtlIHRvIGdpdmUgbXkgc2luY2VyZXN0IHRoYW5rcyBmb3IgeW91ciBpbnRlcmVzdCBpbiB3aGF0YWJ1Zy5jb20sIGlmIHlvdSBhcmUgaW50ZXJlc3RlZCBpbiBjb250cmlidXRpbmcgYW5kIHNoYXBpbmcgdGhpcyB3ZWJzaXRlIHR5cGUgImhpbnQiIGZvciBmb3IgZmluYWwgaGludA=='); 
+// });
+
+// Mousetrap.bind('h i n t', function() { 
+//   document.body.innerHTML = '<p><a href="https://github.com/bondarewicz/whatabug/issues/1">click me<a> <span>&#x2588;</span></p>';
+//   console.clear();
+// });
+
+// http://keycode.info
+var map = {70: false, 73: false, 88: false, 77: false, 69: false, 72: false, 78: false, 84: false};
+
+function KeyPress(e) {
+  console.log(map)
+			
+      
+      var evtobj = window.event? event : e
+      if (e.keyCode in map) {
+        map[e.keyCode] = true;
+        
+        // fixme
+        if (map[70] && map[73] && map[88] && map[77] && map[69]) {
+            document.body.innerHTML = '<p>check the console <span>&#x2588;</span></p>';
+            console.log('SSB3b3VsZCBsaWtlIHRvIGdpdmUgbXkgc2luY2VyZXN0IHRoYW5rcyBmb3IgeW91ciBpbnRlcmVzdCBpbiB3aGF0YWJ1Zy5jb20sIGlmIHlvdSBhcmUgaW50ZXJlc3RlZCBpbiBjb250cmlidXRpbmcgYW5kIHNoYXBpbmcgdGhpcyB3ZWJzaXRlIHR5cGUgImhpbnQiIGZvciBmb3IgZmluYWwgaGludA=='); 
+        }
+        
+        // hint
+        if(map[72] && map[73] && map[78] && map[84]) {
+          document.body.innerHTML = '<p><a href="https://github.com/bondarewicz/whatabug/issues/1">click me<a> <span>&#x2588;</span></p>';
+          console.clear();
+        }
+    }
+}
+
+function reset(e) {
+  console.log(e.keyCode)
+  if (e.keyCode in map) {
+        map[e.keyCode] = false;
+    }
+}
+
+// document.onkeydown = KeyPress;
+document.addEventListener("keydown", KeyPress);
+// document.addEventListener("keyup", reset);
